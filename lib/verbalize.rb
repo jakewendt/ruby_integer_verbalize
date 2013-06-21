7e6350e6e4b5caf4005b290c7066973059cd3f55
Jake <github@jakewendt.com>
2013-06-21 13:40:52 -0700

    Working

--- lib/verbalize.rb
+++ lib/verbalize.rb
@@ -1,5 +1,97 @@
 class Integer
 	def verbalize
-		'hello'
+		if self > 999999999999999
+			"Wow! That's BIG!"
+		else
+			case
+
+
+
+				when( self >= 1000000000000 and self <= 999999999999999 )
+					digits = sprintf("%15d",self)
+					s = [digits[0..2].to_i.verbalize,'trillion']
+					s << digits[3..-1].to_i.verbalize if digits[3..-1].to_i > 0
+					s.join(' ')
+
+				when( self >= 1000000000 and self <= 999999999999 )
+					digits = sprintf("%12d",self)
+					s = [digits[0..2].to_i.verbalize,'billion']
+					s << digits[3..-1].to_i.verbalize if digits[3..-1].to_i > 0
+					s.join(' ')
+
+				when( self >= 1000000 and self <= 999999999 )
+					digits = sprintf("%09d",self)
+					s = [digits[0..2].to_i.verbalize,'million']
+					s << digits[3..-1].to_i.verbalize if digits[3..-1].to_i > 0
+					s.join(' ')
+
+				when( self >= 1000 and self <= 999999 )
+					digits = sprintf("%06d",self)
+					s = [digits[0..2].to_i.verbalize,'thousand']
+					s << digits[3..-1].to_i.verbalize if digits[3..-1].to_i > 0
+					s.join(' ')
+
+
+
+
+
+
+				when( self >= 100 and self <= 999 )
+					s = [self.to_s[0].to_i.verbalize,'hundred']
+					s << self.to_s[1..2].to_i.verbalize if self.to_s[1..2].to_i > 0
+					s.join(' ')
+
+				when self >= 91 then "ninety #{(self-90).verbalize}"
+				when self == 90 then 'ninety'
+				when self >= 81 then "eighty #{(self-80).verbalize}"
+				when self == 80 then 'eighty'
+				when self >= 71 then "seventy #{(self-70).verbalize}"
+				when self == 70 then 'seventy'
+				when self >= 61 then "sixty #{(self-60).verbalize}"
+				when self == 60 then 'sixty'
+				when self >= 51 then "fifty #{(self-50).verbalize}"
+				when self == 50 then 'fifty'
+				when self >= 41 then "forty #{(self-40).verbalize}"
+				when self == 40 then 'forty'
+				when self >= 31 then "thirty #{(self-30).verbalize}"
+				when self == 30 then 'thirty'
+				when self >= 21 then "twenty #{(self-20).verbalize}"
+				when self == 20 then 'twenty'						#	don't want "twenty zero"
+
+#				when self == 0 then 'zero'
+#				when self == 1 then 'one'
+#				when self == 2 then 'two'
+#				when self == 3 then 'three'
+#				when self == 4 then 'four'
+#				when self == 5 then 'five'
+#				when self == 6 then 'six'
+#				when self == 7 then 'seven'
+#				when self == 8 then 'eight'
+#				when self == 9 then 'nine'
+#				when self == 10 then 'ten'
+#				when self == 11 then 'eleven'
+#				when self == 12 then 'twelve'
+#				when self == 13 then 'thirteen'
+#				when self == 14 then 'fourteen'
+#				when self == 15 then 'fifteen'
+#				when self == 16 then 'sixteen'
+#				when self == 17 then 'seventeen'
+#				when self == 18 then 'eighteen'
+#				when self == 19 then 'nineteen'
+				else
+					%w( zero one two three four five six seven eight nine ten eleven twelve thirteen 
+						fourteen fifteen sixteen seventeen eighteen nineteen )[self]
+			end
+
+		end
 	end
 end
+__END__
+
+my first thought is that the number will have to be broken into groups of 3 as that is the magic line
+
+
+
+
+
+
--- test/verbalize_test.rb
+++ test/verbalize_test.rb
@@ -6,7 +6,140 @@ class VerbalizeTest < Test::Unit::TestCase
 		assert 1.respond_to?(:verbalize)
 	end
 
-	test 'that' do
+	test '0 should return zero' do
+		assert_equal 'zero', 0.verbalize
+	end
+
+	test '1 should return one' do
+		assert_equal 'one', 1.verbalize
+	end
+
+	test '10 should return ten' do
+		assert_equal 'ten', 10.verbalize
+	end
+
+	test '19 should return nineteen' do
+		assert_equal 'nineteen', 19.verbalize
+	end
+
+	test '20 should return twenty' do
+		assert_equal 'twenty', 20.verbalize
+	end
+
+	test '21 should return twenty one' do
+		assert_equal 'twenty one', 21.verbalize
+	end
+
+	test '30 should return thirty' do
+		assert_equal 'thirty', 30.verbalize
+	end
+
+	test '31 should return thirty one' do
+		assert_equal 'thirty one', 31.verbalize
+	end
+
+	test '40 should return forty' do
+		assert_equal 'forty', 40.verbalize
+	end
+
+	test '41 should return forty one' do
+		assert_equal 'forty one', 41.verbalize
+	end
+
+	test '50 should return fifty' do
+		assert_equal 'fifty', 50.verbalize
+	end
+
+	test '51 should return fifty one' do
+		assert_equal 'fifty one', 51.verbalize
+	end
+
+	test '60 should return sixty' do
+		assert_equal 'sixty', 60.verbalize
+	end
+
+	test '61 should return sixty one' do
+		assert_equal 'sixty one', 61.verbalize
+	end
+
+	test '70 should return seventy' do
+		assert_equal 'seventy', 70.verbalize
+	end
+
+	test '71 should return seventy one' do
+		assert_equal 'seventy one', 71.verbalize
+	end
+
+	test '80 should return eighty' do
+		assert_equal 'eighty', 80.verbalize
+	end
+
+	test '81 should return eighty one' do
+		assert_equal 'eighty one', 81.verbalize
+	end
+
+	test '90 should return ninety' do
+		assert_equal 'ninety', 90.verbalize
+	end
+
+	test '91 should return ninety one' do
+		assert_equal 'ninety one', 91.verbalize
+	end
+
+	test '99 should return ninety nine' do
+		assert_equal 'ninety nine', 99.verbalize
+	end
+
+	test '100 should return one hundred' do
+		assert_equal 'one hundred', 100.verbalize
+	end
+
+	test '200 should return two hundred' do
+		assert_equal 'two hundred', 200.verbalize
+	end
+
+	test '900 should return nine hundred' do
+		assert_equal 'nine hundred', 900.verbalize
+	end
+
+	test '999 should return nine hundred ninety nine' do
+		assert_equal 'nine hundred ninety nine', 999.verbalize
+	end
+
+	test '1000 should return one thousand' do
+		assert_equal 'one thousand', 1000.verbalize
+	end
+
+	test '10000 should return ten thousand' do
+		assert_equal 'ten thousand', 10000.verbalize
+	end
+
+	test '100000 should return one hundred thousand' do
+		assert_equal 'one hundred thousand', 100000.verbalize
+	end
+
+	test '999999 should return nine hundred ninety nine thousand nine hundred ninety nine' do
+		assert_equal 'nine hundred ninety nine thousand nine hundred ninety nine', 999999.verbalize
+	end
+
+	test '1000000 should return one million' do
+		assert_equal 'one million', 1000000.verbalize
+	end
+
+	test '999999999 should return nine hundred ninety nine million nine hundred ninety nine thousand nine hundred ninety nine' do
+		assert_equal 'nine hundred ninety nine million nine hundred ninety nine thousand nine hundred ninety nine', 999999999.verbalize
+	end
+
+	test '1000000000 should return one billion' do
+		assert_equal "one billion", 1000000000.verbalize
+	end
+
+	test '1000000000000 should return one trillion' do
+		assert_equal "one trillion", 1000000000000.verbalize
+	end
+
+	test '1000000000000000 should return wow thats big' do
+		assert_equal "Wow! That's BIG!", 1000000000000000.verbalize
 	end
 
 end
